00a74cb2210e051476fbc3712377bad09a306c93
bhavanidupaty <dupatybhavani@gmail.com>
2021-07-11 00:20:17 +0530

    second commit with changes

--- src/main/java/testsuite/WalletHub_TestSuite.java
+++ src/main/java/testsuite/WalletHub_TestSuite.java
@@ -6,6 +6,10 @@ import org.testng.annotations.Test;
 public class WalletHub_TestSuite extends BrowserInitialization{
 
 /*
+1. Clicks Login button on Homepage
+2. Login to WalletHub with valid light user credentials
+3. On Test Insurance Company, Clicking on reviews -> Writing a Review for Health Insurance policy
+4. Verifying review on userProfile on successful submission
 */
 
     @Test(description = "Writing a review for WalletHub")
